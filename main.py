2eb0a0bbb0c7f85f215f82b183ef6e4f9d3eb790
Shan Padayhag <shanpadayhag@gmail.com>
2023-08-09 23:04:14 +0800

    Added main scheduling

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from argparse import ArgumentParser
-from smart_schedule import schedule_notion_to_google
+from src.controllers.smart_schedule.smart_schedule_controller import scheduleTasksForDays, scheduleTasksToday
 
 parser = ArgumentParser()
 
 parser.add_argument("--today", action="store_true", help="start scheduling events today")
-parser.add_argument("--days", type=int, default=1, help="number of days to schedule events")
+parser.add_argument("--days", type=int, default=0, help="number of days to schedule events")
 parser.add_argument("--reschedule", action="store_true", help="reschedule events if already scheduled")
-parser.add_argument("--delete", action="store_true", help="delete events if in calendar, but do not schedule anything.")
-parser.add_argument("--force", action="store_true", help="don't ask for confirmation before rescheduling events")
+# parser.add_argument("--delete", action="store_true", help="delete events if in calendar, but do not schedule anything.")
+# parser.add_argument("--force", action="store_true", help="don't ask for confirmation before rescheduling events")
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    schedule_notion_to_google(args.today, args.days, args.reschedule, args.delete)
+    if args.days >= 1:
+        scheduleTasksForDays(days= args.days, reschedule=args.reschedule)
+    else:
+        scheduleTasksToday(reschedule=args.reschedule)
+
---
+++ src/controllers/smart_schedule/smart_schedule_controller.py
@@ -0,0 +1,6 @@
+def scheduleTasksToday(reschedule: bool):
+    print('Scheduling tasks for today')
+
+def scheduleTasksForDays(days: int, reschedule: bool):
+    print('Scheduling tasks for {} days'.format(days))
+
